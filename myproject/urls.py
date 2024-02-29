68eda171d59ae9de9ae4b9aa6caa24dddfdbef35
yawara173946825 <yawara173946825@gmail.com>
2024-02-29 13:42:11 +0900

    投稿一覧表示

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
 
 if settings.DEBUG:
