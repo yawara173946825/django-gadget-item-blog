ca1d418ba93be368abe9e6f9f6e6a68d28066e70
yawara173946825 <yawara173946825@gmail.com>
2024-02-29 20:13:23 +0900

    記事詳細画面実装

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blog.views import PostListView
+from blog.views import PostListView, PostDetailView
 
 app_name = 'blog'
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
 ]
